3f78f1af82e1e1fe833f558abae71ea3e72e7560
onraj55555 <jarnovanruymbeke@gmail.com>
2025-05-21 14:47:34 +0200

    removed clap

--- native/ping-pong/src/main.rs
+++ native/ping-pong/src/main.rs
@@ -1,22 +1,8 @@
-use clap::Parser;
-
-#[derive(clap::Parser)]
-struct Args {
-    
-    #[arg(short('i'), long("input"), required(true))]
-    input: u8,
-    
-    #[arg(short('o'), long("output"), required(true))]
-    output: u8
-}
-
 fn main() {
-    let args = Args::parse();
-    
     let gpio = rppal::gpio::Gpio::new().unwrap();
 
-    let rx = gpio.get(args.input).unwrap().into_input();
-    let mut tx = gpio.get(args.output).unwrap().into_output_low();
+    let rx = gpio.get(2).unwrap().into_input();
+    let mut tx = gpio.get(3).unwrap().into_output_low();
 
     loop {
         while rx.is_low() {}
